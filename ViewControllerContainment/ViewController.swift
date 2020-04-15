363b4a5b44c76943a9711fcb027ef1c54afd219b
russell-archer <russell.archer@mac.com>
2020-04-15 15:51:17 +0100

    Initial Commit

---
+++ ViewControllerContainment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllerContainment
+//
+//  Created by Russell Archer on 15/04/2020.
+//  Copyright © 2020 Russell Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
